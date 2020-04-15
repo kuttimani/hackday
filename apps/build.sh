a7aaf9e1af316dbfb7e68002c7ecd870c87e523a
kutraj01 <kuttimani.rajamani@arm.com>
2020-04-15 11:36:46 +0100

    changes

--- apps/build.sh
+++ apps/build.sh
@@ -3,7 +3,7 @@ source ~/.bashrc &>/dev/null
 IMAGE_VERSION=`grep -hnr "ENV" ./Dockerfile | cut -d'=' -f 2`
 REPO=kuttimani
 IMAGE_NAME=flaskdemo
-docker build -t ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION} .
+docker build --no-cache -t ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION} .
 echo "execute foll. command to push image to dockerhub repo: docker push ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}"
 docker push ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}
 

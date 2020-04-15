00223fb88e2a8aa086680bcdcb96f19bc251fd66
kutraj01 <kuttimani.rajamani@arm.com>
2020-04-15 11:20:50 +0100

    .

--- apps/build.sh
+++ apps/build.sh
@@ -4,3 +4,4 @@ REPO=kuttimani
 IMAGE_NAME=flaskdemo
 docker build -t ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION} .
 echo "execute foll. command to push image to dockerhub repo: docker push ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}"
+docker push ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}

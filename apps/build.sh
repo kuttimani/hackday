b18d75443c2190cfb1daeca0d58bde5671d061a4
kutraj01 <kuttimani.rajamani@arm.com>
2020-04-15 11:25:53 +0100

    .

--- apps/build.sh
+++ apps/build.sh
@@ -1,7 +1,11 @@
 # image version 0.1
+source ~/.bashrc &>/dev/null
 IMAGE_VERSION=`grep -hnr "ENV" ./Dockerfile | cut -d'=' -f 2`
 REPO=kuttimani
 IMAGE_NAME=flaskdemo
 docker build -t ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION} .
 echo "execute foll. command to push image to dockerhub repo: docker push ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}"
 docker push ${REPO}/${IMAGE_NAME}:${IMAGE_VERSION}
+
+echo "########  deploying to kubernetes ########"
+kubectl set image deployment/flask-deployment flask=kuttimani/flask:${IMAGE_VERSION} --record
